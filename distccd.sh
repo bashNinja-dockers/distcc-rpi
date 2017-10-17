20d246071c3cd08fa26bf77517bb07d1dd2eefce
Mike Weaver <miketweaver@users.noreply.github.com>
2017-10-17 10:23:44 -0600

    Update distccd.sh

--- distccd.sh
+++ distccd.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-exec distccd --jobs 7 --allow 172.16.0.0/12 --allow 192.168.0.0/16 --allow 10.0.0.0/8 --log-stderr --no-detach  >>/var/log/distccd.log 2>&1
+exec distccd --jobs 30 --allow 172.16.0.0/12 --allow 192.168.0.0/16 --allow 10.0.0.0/8 --log-stderr --no-detach  >>/var/log/distccd.log 2>&1
